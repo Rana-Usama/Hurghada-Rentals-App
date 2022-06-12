177dc05ac86b33fce3975430f3d77b74ba9d60e5
RANA-USAMA <ranausamauet3430@gmail.com>
2022-06-12 16:44:35 +0500

    wlecome half

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, ScrollView, Image, ImageBackground, StyleSheet } from 'react-native'
 import { RFPercentage } from 'react-native-responsive-fontsize';
+import { LinearGradient } from 'expo-linear-gradient';
 
 //components
 import Screen from '../components/Screen';
@@ -12,9 +13,10 @@ function WelcomeScreen(props) {
 
     return (
         <Screen style={styles.screen}>
-            <Text>
-                Welcome Screen
-            </Text>
+            <Image source={require('../../assets/images/logo.png')} style={{ marginTop: RFPercentage(6), width: RFPercentage(30), height: RFPercentage(12) }} />
+            <LinearGradient colors={['#38EF7D', '#11998E']} start={[1, 1]} end={[0.5, 0.1]} style={{ position: 'absolute', bottom: 0, borderTopLeftRadius: RFPercentage(4), borderTopRightRadius: RFPercentage(4), alignSelf: 'center', alignItems: "center", flexDirection: 'row', justifyContent: 'center', width: '100%', height: RFPercentage(70) }} >
+
+            </LinearGradient>
         </Screen>
     );
 }
@@ -25,6 +27,9 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
         alignItems: "center",
         backgroundColor: Colors.white
+    },
+    linearGradient: {
+
     }
 })
 
